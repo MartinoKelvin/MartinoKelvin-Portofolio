b46ca06149e08fdaa85fbcd1f031f12db27efe2a
MartinoKelvin <martinokelvin06032005@gmail.com>
2026-03-02 00:13:15 +0700

    update title meta data for seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "Martino Kelvin",
   description: "Aesthetically pleasing gray themed portfolio",
 };
 
